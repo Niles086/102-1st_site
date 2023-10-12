3f15621c0bb739aa6020e575d338148a3dd49cf1
Niles D Thompson <dewand.th@gmail.com>
2023-10-11 17:50:08 -0700

    centered photos on page

---
+++ ageverification.js
@@ -0,0 +1,11 @@
+let age = prompt('You must be 21 years or older enter this site. How old are you?');
+if(age >= 21){
+    documenw.write('Welcome to Anime Addict');
+    } else {
+        function redirectToAnotherSite() 
+        window.location.href = "https://www.babiesrus.com/"
+    }
+    
+
+
+    window.location.href = "https://niles086.github.io/cf-projects/"; 
